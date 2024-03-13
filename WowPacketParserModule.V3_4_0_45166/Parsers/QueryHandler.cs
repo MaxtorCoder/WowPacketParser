09619c4c88ea731894b996573bd1d7bc4da77571
MaxtorCoder <warsongkiller.s8@gmail.com>
2024-03-13 19:44:32 +0100

    Fix SMSG_QUERY_CREATURE_RESPONSE

--- WowPacketParserModule.V3_4_0_45166/Parsers/QueryHandler.cs
+++ WowPacketParserModule.V3_4_0_45166/Parsers/QueryHandler.cs
@@ -13,29 +13,29 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
         [Parser(Opcode.SMSG_QUERY_CREATURE_RESPONSE)]
         public static void HandleCreatureQueryResponse(Packet packet)
         {
-            PacketQueryCreatureResponse response = packet.Holder.QueryCreatureResponse = new PacketQueryCreatureResponse();
+            var response = packet.Holder.QueryCreatureResponse = new PacketQueryCreatureResponse();
             var entry = packet.ReadEntry("Entry");
 
-            CreatureTemplate creature = new CreatureTemplate
+            var creature = new CreatureTemplate
             {
                 Entry = (uint)entry.Key
             };
             response.Entry = (uint)entry.Key;
 
-            Bit hasData = packet.ReadBit();
+            var hasData = packet.ReadBit();
             response.HasData = hasData;
             if (!hasData)
                 return; // nothing to do
 
             packet.ResetBitReader();
-            uint titleLen = packet.ReadBits(11);
-            uint titleAltLen = packet.ReadBits(11);
-            uint cursorNameLen = packet.ReadBits(6);
+            var titleLen = packet.ReadBits(11);
+            var titleAltLen = packet.ReadBits(11);
+            var cursorNameLen = packet.ReadBits(6);
             creature.Civilian = packet.ReadBit("Civilian");
             creature.RacialLeader = response.Leader = packet.ReadBit("Leader");
 
             var stringLens = new int[4][];
-            for (int i = 0; i < 4; i++)
+            for (var i = 0; i < 4; i++)
             {
                 stringLens[i] = new int[2];
                 stringLens[i][0] = (int)packet.ReadBits(11);
@@ -46,13 +46,13 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             {
                 if (stringLens[i][0] > 1)
                 {
-                    string name = packet.ReadDynamicString("Name", stringLens[i][0], i);
+                    var name = packet.ReadDynamicString("Name", stringLens[i][0], i);
                     if (i == 0)
                         creature.Name = response.Name = name;
                 }
                 if (stringLens[i][1] > 1)
                 {
-                    string nameAlt = packet.ReadDynamicString("NameAlt", stringLens[i][1], i);
+                    var nameAlt = packet.ReadDynamicString("NameAlt", stringLens[i][1], i);
                     if (i == 0)
                         creature.FemaleName = response.NameAlt = nameAlt;
                 }
@@ -71,7 +71,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             response.Rank = (int?)creature.Rank ?? 0;
 
             creature.KillCredits = new uint?[2];
-            for (int i = 0; i < 2; ++i)
+            for (var i = 0; i < 2; ++i)
             {
                 creature.KillCredits[i] = (uint)packet.ReadInt32("ProxyCreatureID", i);
                 response.KillCredits.Add(creature.KillCredits[i] ?? 0);
@@ -82,7 +82,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             for (var i = 0; i < displayIdCount; ++i)
             {
-                CreatureTemplateModel model = new CreatureTemplateModel
+                var model = new CreatureTemplateModel
                 {
                     CreatureID = (uint)entry.Key,
                     Idx = (uint)i
@@ -98,7 +98,10 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             creature.HealthModifier = response.HpMod = packet.ReadSingle("HpMulti");
             creature.ManaModifier = response.ManaMod = packet.ReadSingle("EnergyMulti");
-            uint questItems = packet.ReadUInt32("QuestItems");
+            var questItems = packet.ReadUInt32("QuestItems");
+            var questCurrencies = 0u;
+            if (ClientVersion.AddedInVersion(ClientBranch.Cata, ClientVersionBuild.V4_4_0_53627))
+                questCurrencies = packet.ReadUInt32("QuestCurrencies");
             creature.MovementID = response.MovementId = (uint)packet.ReadInt32("CreatureMovementInfoID");
             creature.HealthScalingExpansion = packet.ReadInt32E<ClientType>("HealthScalingExpansion");
             response.HpScalingExp = (uint?)creature.HealthScalingExpansion ?? 0;
@@ -121,7 +124,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             for (uint i = 0; i < questItems; ++i)
             {
-                CreatureTemplateQuestItem questItem = new CreatureTemplateQuestItem
+                var questItem = new CreatureTemplateQuestItem
                 {
                     CreatureEntry = (uint)entry.Key,
                     Idx = i,
@@ -131,12 +134,24 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
                 Storage.CreatureTemplateQuestItems.Add(questItem, packet.TimeSpan);
                 response.QuestItems.Add(questItem.ItemId ?? 0);
             }
+            
+            for (uint i = 0; i < questCurrencies; ++i)
+            {
+                var questCurrency = new CreatureTemplateQuestCurrency
+                {
+                    CreatureId = (uint)entry.Key,
+                    CurrencyId = packet.ReadInt32<CurrencyId>("QuestCurrency", i)
+                };
+
+                Storage.CreatureTemplateQuestCurrencies.Add(questCurrency, packet.TimeSpan);
+                response.QuestCurrencies.Add(questCurrency.CurrencyId ?? 0);
+            }
 
             packet.AddSniffData(StoreNameType.Unit, entry.Key, "QUERY_RESPONSE");
 
             if (ClientLocale.PacketLocale != LocaleConstant.enUS)
             {
-                CreatureTemplateLocale localesCreature = new CreatureTemplateLocale
+                var localesCreature = new CreatureTemplateLocale
                 {
                     ID = (uint)entry.Key,
                     Name = creature.Name,
@@ -150,7 +165,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             Storage.CreatureTemplates.Add(creature.Entry.Value, creature, packet.TimeSpan);
 
-            ObjectName objectName = new ObjectName
+            var objectName = new ObjectName
             {
                 ObjectType = StoreNameType.Unit,
                 ID = entry.Key,
@@ -203,7 +218,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             packet.ReadPackedGuid128("GUID");
 
-            GameObjectTemplate gameObject = new GameObjectTemplate
+            var gameObject = new GameObjectTemplate
             {
                 Entry = (uint)entry.Key
             };
@@ -211,7 +226,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             packet.ReadBit("Allow");
 
-            int dataSize = packet.ReadInt32("DataSize");
+            var dataSize = packet.ReadInt32("DataSize");
             query.HasData = dataSize > 0;
             if (dataSize == 0)
                 return;
@@ -221,7 +236,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             gameObject.DisplayID = (uint)packet.ReadInt32("Display ID");
 
             var name = new string[4];
-            for (int i = 0; i < 4; i++)
+            for (var i = 0; i < 4; i++)
                 name[i] = packet.ReadCString("Name", i);
             gameObject.Name = name[0];
 
@@ -230,15 +245,15 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
             gameObject.UnkString = packet.ReadCString("Unk String");
 
             gameObject.Data = new int?[35];
-            for (int i = 0; i < gameObject.Data.Length; i++)
+            for (var i = 0; i < gameObject.Data.Length; i++)
                 gameObject.Data[i] = packet.ReadInt32("Data", i);
 
             gameObject.Size = packet.ReadSingle("Size");
 
-            byte questItemsCount = packet.ReadByte("QuestItemsCount");
+            var questItemsCount = packet.ReadByte("QuestItemsCount");
             for (uint i = 0; i < questItemsCount; i++)
             {
-                GameObjectTemplateQuestItem questItem = new GameObjectTemplateQuestItem
+                var questItem = new GameObjectTemplateQuestItem
                 {
                     GameObjectEntry = (uint)entry.Key,
                     Idx = i,
@@ -255,7 +270,7 @@ namespace WowPacketParserModule.V3_4_0_45166.Parsers
 
             Storage.GameObjectTemplates.Add(gameObject, packet.TimeSpan);
 
-            ObjectName objectName = new ObjectName
+            var objectName = new ObjectName
             {
                 ObjectType = StoreNameType.GameObject,
                 ID = entry.Key,
